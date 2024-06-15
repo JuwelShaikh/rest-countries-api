920a8d0b082050c78ce2151288dabdcaa12c5249
JuwelShaikh <juwelshaikh04@gmail.com>
2024-06-15 23:30:45 +0600

    Flags showed

--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -1,21 +1,25 @@
 
 
 const addStyle = {
-    width: '500px',
+    width: '400px',
     border: '5px solid blue',
     borderRadius: "10px",
     margin: '5px'
 }
 
+const imgStyle = {
+    with:'100px',
+    marginBottom: '10px'
+}
 
 const Country = ({ country }) => {
-    const { name, capital, languages } = country;
+    const { name, flags } = country;
     console.log(country)
     return (
         <div style={addStyle}>
             <h4>Name: <u>{name.common}</u></h4>
-            <p>Capital: {capital}</p>
-            <p>Language: {languages}</p>
+            <img style={imgStyle} src={flags.png} alt="" />
+
         </div>
     );
 };
