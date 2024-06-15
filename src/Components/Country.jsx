b6652c99044e8705f590eeb68501122bb6afd819
JuwelShaikh <juwelshaikh04@gmail.com>
2024-06-15 23:22:59 +0600

    ...

--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -1,17 +1,21 @@
 
 
 const addStyle = {
-    // backgroundColor: 'blue',
-    border: '10px blue',
-    // borderRedious: "10px"
-
+    width: '500px',
+    border: '5px solid blue',
+    borderRadius: "10px",
+    margin: '5px'
 }
 
+
 const Country = ({ country }) => {
+    const { name, capital, languages } = country;
+    console.log(country)
     return (
         <div style={addStyle}>
-            <h4>Name: </h4>
-            <p>Capital: </p>
+            <h4>Name: <u>{name.common}</u></h4>
+            <p>Capital: {capital}</p>
+            <p>Language: {languages}</p>
         </div>
     );
 };
