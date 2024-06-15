aa14fc15677432c2b42572bba1e0b63855595312
JuwelShaikh <juwelshaikh04@gmail.com>
2024-06-15 13:56:58 +0600

    Api loadded

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Countries from './Components/Countries'
 
 function App() {
   
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <h1>World Tour!</h1>
+      <Countries/>
     </>
   )
 }
